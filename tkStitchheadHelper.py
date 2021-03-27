36f7660b0beaf52e2f1d3aed17a5c5c305acc953
Thomas Kutschera <tkutsch99@gmail.com>
2021-03-27 17:44:51 +0100

    Update tkStitchheadHelper.py

--- tkStitchheadHelper.py
+++ tkStitchheadHelper.py
@@ -5,7 +5,8 @@ import maya.cmds as cmds
 import maya.mel as mel
 
 def cImportGuides(*args):
-    cmd1 = 'file -import -type "mayaAscii"  -ignoreVersion -mergeNamespacesOnClash false -rpr "_guides" -options "v=0;"  -pr  -importFrameRate true  -importTimeRange "override" "X:/stitchhead_shd-5070/_sandbox/thomas.kutschera/assets/_guides.ma"'
+    # cmd1 = 'file -import -type "mayaAscii"  -ignoreVersion -mergeNamespacesOnClash false -rpr "_guides" -options "v=0;"  -pr  -importFrameRate true  -importTimeRange "override" "X:/stitchhead_shd-5070/_sandbox/thomas.kutschera/assets/_guides.ma"'
+    cmd1 = 'file -import -type "mayaAscii" -ignoreVersion -mergeNamespacesOnClash false -rpr "_guides" -options "v=0;"  -pr  -importFrameRate true -importTimeRange "override" "C:/gitRepository/tkStitchHead/_guides.ma"'
     mel.eval(cmd1)
     
     
@@ -40,6 +41,25 @@ def cAddStitchLocs(*args):
         print ('missing elements :(')    
     
     
+def cFindEmptyTransforms(*args):
+    dags = cmds.ls(l=1, type='transform')
+    for dag in dags:
+        shps = cmds.listRelatives(dag, s=1)
+        if shps == 'None':
+            ending = dag.split('_')[-1]
+            if ending != 'GRUP':
+                cmds.select(dag, r=1)
+                print 'ending: ' + ending
+                cmd1 = 'searchReplaceNames("'
+                cmd2 = '", "'
+                cmd3 = '", "selected")'
+                cmd = cmd1 + ending + cmd2 + 'GRUP' + cmd3
+                print cmd
+                mel.eval(cmd)
+        # else:
+            # print 'has shapes'
+
+
 def cAddAttributes(*args):
     carries = ['world_C_001_CTRL', 'global_C_001_CTRL',  'local_C_001_CTRL']
     carryWorld = carries[0]
@@ -47,32 +67,33 @@ def cAddAttributes(*args):
     carryLocal = carries[2]
     geoGrp = 'geo_C_001_GRUP'
     ctlGrp = 'cogRoot_C_001_GRUP'
-    # gloSclMult = 'globalScaleOverride_C_001_RMDI'
-    gloSclMult =  cmds.shadingNode('multiplyDivide', asUtility=1, n=gloSclMult)
+    gloSclMult =  cmds.shadingNode('multiplyDivide', asUtility=1, n='globalScaleOverride_C_001_RMDI')
     
     # create nodes
     # cmds.shadingNode('multiplyDivide', asUtility=1, n=gloSclMult)
     
-    cmds.setAttr(gloSclMult + '.imput2X', 1)
-    cmds.setAttr(gloSclMult + '.imput2Y', 1)
-    cmds.setAttr(gloSclMult + '.imput2Z', 1)
+    cmds.setAttr(gloSclMult + '.input2X', 1)
+    cmds.setAttr(gloSclMult + '.input2Y', 1)
+    cmds.setAttr(gloSclMult + '.input2Z', 1)
     cmds.setAttr(gloSclMult + '.operation', 1)
 
     
     # add attributes
     for carry in carries:
-        cmds.addAttr(carry, sn='___', ln='__________', at='enum', en='GLOBAL')
-        cmds.setAttr(carry + '.___', e=1, displayable=1)
+        print carry
+        print 'world'
+        cmds.addAttr(carry, sn='_', ln='___________', at='enum', en='GLOBAL')
+        cmds.setAttr(carry + '._', e=1,  k=0, cb=1)
         
-        cmds.addAttr(carry, sn='___', ln='__________', at='enum', en='GLOBAL')
-        cmds.setAttr(carry + '.___', e=1, displayable=1)
-        
+        print 'global scale'
         cmds.addAttr(carry, ln='globalScale', at='double', dv=1, min=.1) 
         cmds.setAttr(carry + '.globalScale', e=1, keyable=1)
         
-        cmds.addAttr(carry, sn='____', ln='__________', at='enum', en='DISPLAY')
-        cmds.setAttr(carry + '.____', e=1, displayable=1)
+        print 'display'
+        cmds.addAttr(carry, sn='__', ln='____________', at='enum', en='DISPLAY')
+        cmds.setAttr(carry + '.__', e=1,  k=0, cb=1)
         
+        print 'ctrl display'
         cmds.addAttr(carry, ln='ctrlDisplay', at='bool', dv=1, min=.1) 
         cmds.setAttr(carry + '.ctrlDisplay', e=1, keyable=1)
         
@@ -91,9 +112,9 @@ def cAddAttributes(*args):
         
         
     # connect attributes
-    cmds.connectAttr(carryLocal + '.scaleX', gloSclMult + '.input1X', f=1)
-    cmds.connectAttr(carryLocal + '.scaleY', gloSclMult + '.input1Y', f=1)
-    cmds.connectAttr(carryLocal + '.scaleZ', gloSclMult + '.input1Z', f=1)
+    cmds.connectAttr(carryLocal + '.scaleX', gloSclMult + '.input1.input1X', f=1)
+    cmds.connectAttr(carryLocal + '.scaleY', gloSclMult + '.input1.input1Y', f=1)
+    cmds.connectAttr(carryLocal + '.scaleZ', gloSclMult + '.input1.input1Z', f=1)
     cmds.setAttr (geoGrp + '.overrideEnabled', 1)
     cmds.connectAttr(carryLocal + '.ctrlDisplay', ctlGrp + '.visibility', f=1)
     cmds.connectAttr(carryLocal + '.ctrlHideOnPlay', ctlGrp + '.hideOnPlayback', f=1)
@@ -107,9 +128,12 @@ def cAddAttributes(*args):
 
 
 def cRenameNodes(*args):
-    nameList = ['grp', 'GRUP', 'GRP', 'GRUP', 'ctl', 'CTRL', 'CON', 'CTRL', 'JNT', 'JOIN', 'jnt', 'JOIN', 
+    nameList = [
+    'grp', 'GRUP', 'GRP', 'GRUP', 'ctl', 'CTRL', 'CON', 'CTRL', 'JNT', 'JOIN', 'jnt', 'JOIN', 
     'parentConstraint1', 'DPAR', 'scaleConstrainta', 'DSCC', 'pointConstraint1', 'DPCC', 'orientConstraint1', 
-    'DOCC', 'root', 'ROOT', '_C1', '_C_001', '_L1', '_L_001', '_R1', '_R_001']
+    'DOCC', 'root', 'ROOT', '_C1_ik_cns', 'IkCns_C_001_GRUP', '_C1_root', 'Root_C_001_GRUP', 
+    '_C1', '_C_001', '_L1', '_L_001', '_R1', '_R_001',
+    ]
     
     for i in range(0,len(nameList), 2):
         cmd1 = 'searchReplaceNames("'
@@ -137,7 +161,8 @@ cmds.button(l='1. Import Guides', c=cImportGuides)
 cmds.button(l='1. Add Special Locs', c=cAddStitchLocs)
 cmds.button(l='3. Rename Nodes', c=cRenameNodes)
 cmds.button(l='4. Add Attributes', c=cAddAttributes)
+cmds.button(l='5. Add GRUP', c=cFindEmptyTransforms)
 
 cmds.showWindow(myWindow)
 
-
+cmds.window(myWindow, w=300, h=100, e=1)
